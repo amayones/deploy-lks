220cc9cceac8360eaaa3703170529a48215b2815
EC2 Default User <ec2-user@ip-10-0-1-66.ec2.internal>
2026-06-02 06:17:59 +0000

    deploy

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('db_lks2026', 'root', 'rahasia123', {
-  host: 'localhost',
+const sequelize = new Sequelize('db_lks2026', 'admin', 'StrongPassword123', {
+  host: 'lks-db.cknm6ci28bfu.us-east-1.rds.amazonaws.com',
   dialect: 'mysql'
 });
 
